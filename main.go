fd1d51412114e636dc25d5de8b00907c45a0efe0
Octavio García Larrecharte <okigarcia06@gmail.com>
2025-04-25 16:28:43 -0300

    adding banking app

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"log"
-
-	"some-time-api/app"
+	"github.com/ashishjuyal/banking/app"
 )
 
 func main() {
-	log.Println("Starting server at http://localhost:8080")
-	if err := app.Run(); err != nil {
-		log.Fatal(err)
-	}
+
+	app.Start()
+
 }
