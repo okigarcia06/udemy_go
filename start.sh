d5b0e6286405b4da12775581985e62933d930453
Octavio García Larrecharte <okigarcia06@gmail.com>
2025-04-25 16:31:22 -0300

    refactor: start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 SERVER_ADDRESS=localhost \
-SERVER_PORT=8000 \
+SERVER_PORT=8080 \
 DB_USER=root \
-DB_PASSWD=codecamp \
+DB_PASSWD=none \
 DB_ADDR=localhost \
 DB_PORT=3306 \
 DB_NAME=banking \
